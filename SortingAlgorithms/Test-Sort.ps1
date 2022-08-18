8f92bb8af3024c1c65f160f0d2d1539d4df454df
aloushin01 <amyloushin@gmail.com>
2022-08-17 19:07:27 -0500

    Create selection sort algorithm script

--- SortingAlgorithms/Test-Sort.ps1
+++ SortingAlgorithms/Test-Sort.ps1
@@ -1,16 +1,30 @@
+<#
+Sorting methods:
+Selection sort - identify smallest in list and move to front. Repeat.
+Bubble sort - compare first two items in list and check if they are in the correct order. If yes, move to next pair. If not, switch them then move to next pair.
+Insertion sort - Go through unsorted items in order and put into their proper spot in the sorted part of the list.
+Merge sort - Divide list in half. Divide each half in half. Repeat until you get down to one item in each half. Sort each pair then start merging pairs together.
+Quick sort - pick an element out of the array. Divide array into two arrays - one with items that are higher and one with items that are lower. Repeat.
+#>
+
 function Test-Sort {
-    $numList = 1..100
+     $inputList = 1..100
+    # $inputList = "apple", "orange", "cat", "tree", "apple"
 
-    $randomList = $numList | Sort-Object {Get-Random}
+    $randomList = $inputList | Sort-Object {Get-Random}
 
-    $sortedList = $randomList | MySort
+    $mySortedList = MySort -array $randomList
 
-    if ((Compare-Object $numList $sortedList).Length -eq 0) {
+    $sortedList = $inputList | Sort-Object
+
+    if ((Compare-Object $mySortedList $sortedList -SyncWindow 0).Length -eq 0) {
         Write-Host "They match!"
     } 
     else {
         Write-Host "No match."
     }
+    Write-Host $mySortedList
+    Write-Host $sortedList
 }
 
 function MySort {
@@ -19,10 +33,53 @@ function MySort {
             Mandatory=$True,
             ValueFromPipeline=$True
         )]
-        [Array]$array
+        $array
     )
 
+    $i = 0
+    # Repeat until list is sorted
+    While ($i -lt $array.Length) {
+        # Identify smallest item in array
+        $minItem = ($array[$i..$array.Length] | Measure-Object -Minimum).Minimum
+
+        # Find the index of the smallest item
+        $index = ($array[$i..$array.Length]).IndexOf($minItem) + $i
+
+        # Swap min item into current index we are sorting
+        if ($i -ne $index) {
+            $array = Swap -array $array -index1 $i -index2 $index
+        }
+
+        $i += 1
+    }
+
     Return $array
+}
+
+function Swap {
+    param (
+        [Parameter(
+            Mandatory=$True
+        )]
+        $array,
+
+        [Parameter(
+            Mandatory=$True
+        )]
+        $index1,
+
+        [Parameter(
+            Mandatory=$True
+        )]
+        $index2
+    )
+    
+    $temp = $array[$index2]
+    $array[$index2] = $array[$index1]
+    $array[$index1] = $temp
+
+    Return $array
+
 }
 
 Test-Sort
