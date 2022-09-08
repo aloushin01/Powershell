6f626e696bdc68560810d0c99f26de81d4a6c2cc
aloushin01 <amyloushin@gmail.com>
2022-09-07 20:28:32 -0500

    Turn FizzBuzz script into function

--- Miscellaneous/FizzBuzz.ps1
+++ Miscellaneous/FizzBuzz.ps1
@@ -1,11 +1,49 @@
-foreach ($i in 1..100) {
- if (($i % 15) -eq 0) {
-    Write-Host "FizzBuzz"
- } elseif (($i % 5) -eq 0) {
-    Write-Host "Buzz"
- } elseif (($i % 3) -eq 0) {
-    Write-Host "Fizz"
- } else {
-    Write-Host $i
- }
+Set-StrictMode -Version Latest
+function FizzBuzzOuter {
+   param (
+      [Parameter(Mandatory=$true)]   
+      [Int[]] $NumArray,
+
+      [Parameter(Mandatory=$true)]
+      [hashtable[]] $ValArray
+   )
+
+   $SortedArray = $ValArray | Sort-Object -Property Divisor -Descending
+
+   foreach ($i in $NumArray) {
+      FizzBuzzInner -Num $i -ValArray $SortedArray
+   }
 }
+
+function FizzBuzzInner {
+   param (
+      [Parameter(Mandatory=$true)]   
+      [Int] $Num,
+
+      [Parameter(Mandatory=$true)]
+      [hashtable[]] $ValArray
+   )
+
+   foreach ($x in $ValArray) {
+      if (($Num % $x.Divisor) -eq 0) {
+         Return $x.Text
+      }
+   }
+   Return $i
+}
+
+$ValArray = @(
+   @{
+      Divisor = 5;
+      Text = "Fizz"
+   },@{
+      Divisor = 3;
+      Text = "Buzz"
+   },@{
+      Divisor = 15;
+      Text = "FizzBuzz"
+   }
+)
+
+FizzBuzzOuter -NumArray (1..100) -ValArray $ValArray
+
